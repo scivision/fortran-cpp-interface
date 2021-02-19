e4808b0be3605cd7c0e808317472cdfd5d66beb4
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-02-19 13:25:00 -0500

    don't repeat abi check

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,12 +1,16 @@
 # check C and Fortran compiler ABI compatibility
 
-try_compile(abi_ok ${CMAKE_CURRENT_BINARY_DIR}/abi_check ${CMAKE_CURRENT_LIST_DIR}/abi_check abi_check)
-if(abi_ok)
-  message(STATUS "C and Fortran compiler detected to be ABI-compatible.")
-else()
-  message(FATAL ERROR "C compiler {CMAKE_C_COMPILER_ID} {CMAKE_C_COMPILER_VERSION} and Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION} are ABI-incompatible.")
+if(NOT abi_ok)
+  message(CHECK_START "checking that C and Fortran compilers can link")
+  try_compile(abi_ok ${CMAKE_CURRENT_BINARY_DIR}/abi_check ${CMAKE_CURRENT_LIST_DIR}/abi_check abi_check)
+  if(abi_ok)
+    message(CHECK_PASS "OK")
+  else()
+    message(FATAL ERROR "C compiler {CMAKE_C_COMPILER_ID} {CMAKE_C_COMPILER_VERSION} and Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION} are ABI-incompatible.")
+  endif()
 endif()
 
+# --- compiler options
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   # this helps show the options are/aren't conflicting between C and Fortran
