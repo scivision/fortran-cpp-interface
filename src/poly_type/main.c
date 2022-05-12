8a5f02dc03d3a4acdda1e50a3333a6a372edcf2f
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-12 13:32:05 -0400

    add poly_type main c

---
+++ src/poly_type/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+extern void init_type(int*, void**);
+extern void add_one_C(int*, void**, int*, int*);
+
+
+int main(){
+
+  void* x3;
+  void* x4;
+  int xtype=3;
+  int A, C;
+
+  xtype = 3;
+  init_type(&xtype, &x3);
+
+  add_one_C(&xtype, &x3, &A, &C);
+  if(A != 4) {
+    fprintf(stderr, "Error: %d != 4\n", A);
+    return 1;
+  }
+  printf("C:3 = %d\n", C);
+  add_one_C(&xtype, &x3, &A, &C);
+  printf("C:3 = %d\n", C);
+  add_one_C(&xtype, &x3, &A, &C);
+  printf("C:3 = %d\n", C);
+
+  xtype = 4;
+  init_type(&xtype, &x4);
+
+  add_one_C(&xtype, &x4, &A, &C);
+  if(A != 5) {
+    fprintf(stderr, "Error: %d != 5\n", A);
+    return 1;
+  }
+  printf("C:4 = %d\n", C);
+  add_one_C(&xtype, &x4, &A, &C);
+  printf("C:4 = %d\n", C);
+  add_one_C(&xtype, &x4, &A, &C);
+  printf("C:4 = %d\n", C);
+
+  printf("OK: poly_type\n");
+
+  return 0;
+}
