01f42f6ed66eeec5bfaf2ce59a4af87aea05e30b
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-13 16:20:46 -0400

    add c malloc

---
+++ src/malloc/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern void timestwo(int [], int [], const size_t*);
+
+
+int main()
+{
+  const size_t N = 3;
+
+  int* x = malloc(N*sizeof(int));
+  int* x2 = malloc(N*sizeof(int));
+
+  timestwo(&x[0], &x2[0], &N);
+
+  for (size_t i=0; i < N; i++){
+    if (x2[i] != 2*x[i]){
+      fprintf(stderr, "value %d != %d\n", x2[i], x[i]);
+      return 1;
+    }
+  }
+
+  free(x);
+  free(x2);
+
+  printf("OK: C malloc new\n");
+
+  return 0;
+}
