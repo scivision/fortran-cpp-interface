307b427cc22811b6f60419bf4aab05aa37f0a979
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-18 12:03:56 -0400

    ABI check work with more compilers

--- src/array/lib.cpp
+++ src/array/lib.cpp
@@ -1,8 +1,6 @@
 #include <cstddef>
 
-extern "C" void timestwo(int*, size_t*);
-
-void timestwo(int* A, size_t* N)
+extern "C" void timestwo(int* A, size_t* N)
 {
   for (auto i=0u; i < *N; i++){
     A[i] = 2*A[i];
