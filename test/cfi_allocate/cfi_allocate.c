74ef1d09c0f590376451addc1c3229dcd9f66d99
scivision <scivision@users.noreply.github.com>
2023-01-29 23:36:29 -0500

    add CFI_is_contiguous

--- test/cfi_allocate/cfi_allocate.c
+++ test/cfi_allocate/cfi_allocate.c
@@ -1,4 +1,5 @@
 // https://stackoverflow.com/a/37837472
+#include <stdio.h>
 #include <ISO_Fortran_binding.h>
 
 void alloc3d(CFI_cdesc_t* x) {
@@ -8,6 +9,10 @@ void alloc3d(CFI_cdesc_t* x) {
   CFI_index_t upper[3]={20, 9, 5};
 
   CFI_allocate(x, lower, upper, 0);
+
+  if (CFI_is_contiguous(x) == 1) {
+    printf("array is contiguous\n");
+  }
 }
 
 void dealloc3d(CFI_cdesc_t* x) {
