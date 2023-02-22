3b2d457f7a3b9996e3d25243560b60ea602c091c
scivision <scivision@users.noreply.github.com>
2023-02-22 02:18:25 -0500

    don't print compiler log

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -8,7 +8,6 @@ if(NOT abi_ok)
   try_compile(abi_ok
   ${CMAKE_CURRENT_BINARY_DIR}/abi_check ${CMAKE_CURRENT_LIST_DIR}/abi_check
   abi_check
-  OUTPUT_VARIABLE abi_log
   )
   if(abi_ok)
     message(CHECK_PASS "OK")
@@ -16,9 +15,7 @@ if(NOT abi_ok)
     message(FATAL_ERROR "ABI-incompatible compilers:
     C compiler ${CMAKE_C_COMPILER_ID} ${CMAKE_C_COMPILER_VERSION}
     C++ compiler ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}
-    Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}
-    ${abi_log}
-    "
+    Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}"
     )
   endif()
 endif()
