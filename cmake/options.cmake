3c5a3f3ad83143f69d8f7d05092467c119b4a057
scivision <scivision@users.noreply.github.com>
2023-08-15 16:26:00 -0400

    don't override rpath

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -6,13 +6,6 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
 endif()
 
-include(GNUInstallDirs)
-
-# Rpath options necessary for shared library install to work correctly in user projects
-set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_FULL_LIBDIR})
-set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_FULL_LIBDIR})
-set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
-
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
 
