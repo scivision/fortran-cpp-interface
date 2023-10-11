8f31ee53bf462342c7797e799f9f73308bf8a38d
scivision <scivision@users.noreply.github.com>
2023-10-11 09:27:00 -0400

    pointer_bounds: print compiler version

--- test/pointer/pointer_bounds.f90
+++ test/pointer/pointer_bounds.f90
@@ -3,10 +3,15 @@ program pointer_bounds
 !! Fortran allows arbitrary lbound, ubound.
 !! In C array index starts at 0, but pointers can start in middle of array and then validly use negative indices.
 use, intrinsic :: iso_c_binding, only : C_INT
+use, intrinsic :: iso_fortran_env
+
 implicit none
 
 integer(C_INT), dimension(-1:2), target :: Ln1
 integer(C_INT), pointer :: Pn1(:)
+
+
+print '(a)', compiler_version()
 
 call Lneg1(Ln1)
 call Lneg1_C(Ln1)
@@ -35,7 +40,13 @@ end subroutine Pneg1
 subroutine Lneg1_C(A) bind(C)
 integer(C_INT), dimension(:) :: A
 print '(a,i0,a,i0)', "Lneg1_C: lbound(A) = ", lbound(A,1), " ubound(A) = ", ubound(A,1)
-if (lbound(A,1) /= 1) error stop "Lneg1_C: unexpected lbound(A,1)"
-if (ubound(A,1) /= 4) error stop "Lneg1_C: unexpected ubound(A,1)"
+if (lbound(A,1) /= 1) then
+  write(error_unit,'(a,i0)') "Lneg1_C: unexpected lbound(A,1) ", lbound(A,1)
+  error stop
+endif
+if (ubound(A,1) /= 4) then
+  write(error_unit, '(a,i0)') "Lneg1_C: unexpected ubound(A,1) ", ubound(A,1)
+  error stop
+endif
 end subroutine lneg1_C
 end program
