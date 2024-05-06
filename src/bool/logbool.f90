93737900deab6e22112a27c7a1a1971ebab2738b
scivision <scivision@users.noreply.github.com>
2024-05-05 23:48:25 -0400

    test:bool: storage_size

--- src/bool/logbool.f90
+++ src/bool/logbool.f90
@@ -10,13 +10,24 @@ logical(c_bool) function logical_not(L) bind(C)
 
 logical(c_bool), intent(in), value :: L
 
+logical :: Ld
+integer :: i
+integer(C_INT) :: ic
+
+!> avoid uninitialized variable warning
+i = 0
+ic = 0
+Ld = .false.
+
 logical_not = .not. L
 
-print '(a,20x,a)', "storage_size() ", "bits"
-print '(a35,i0)', "Fortran: storage_size(logical): ", storage_size(L)
-print '(a35,i0)', "C/C++: storage_size(C_BOOL): ", storage_size(.false.)
-print '(a35,i0)', "Fortran: storage_size(integer): ", storage_size(1)
-print '(a35,i0)', "C/C++: storage_size(C_INT): ", storage_size(1_c_int)
+print '(/, a, l1, a, l1)', "logical_not(", L, "): ", logical_not
+
+print '(a,1x,a)', "storage_size()", "bits"
+print '(a15,i0)', "logical: ", storage_size(Ld)
+print '(a15,i0)', "C_BOOL: ", storage_size(L)
+print '(a15,i0)', "integer: ", storage_size(i)
+print '(a15,i0)', "C_INT: ", storage_size(ic)
 
 end function logical_not
 
