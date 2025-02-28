6cd1b10b045187569f8aa532f5b250f0d032aa34
scivision <scivision@users.noreply.github.com>
2025-02-27 20:48:23 -0500

    Fortran check if C function returned NULL pointer

--- test/time/strptime.f90
+++ test/time/strptime.f90
@@ -10,22 +10,21 @@ integer(C_INT) :: tm_sec=0,tm_min=0,tm_hour=0,tm_mday=0,tm_mon=0,tm_year=0,tm_wd
 end type
 
 interface
-integer(C_INT) function strptime(str,format,tm) bind(C)
-import C_INT, C_CHAR, tm_struct
-character(kind=c_char), intent(in) :: str(*), format(*)
+function strptime(str,format,tm) bind(C)
+import
+character(kind=C_CHAR), intent(in) :: str(*), format(*)
 type(tm_struct), intent(out) :: tm
+type(C_PTR) :: strptime
 end function
 end interface
 
-integer(C_INT) :: rc
-character(kind=c_char, len=20), parameter :: str = '2018-01-01 12:00:00'
-character(kind=c_char, len=18), parameter :: fmt = '%Y-%m-%d %H:%M:%S'
+type(C_PTR) :: rc
+character(kind=C_CHAR, len=20) :: str = '2018-01-01 12:00:00'
+character(kind=C_CHAR, len=18), parameter :: fmt = '%Y-%m-%d %H:%M:%S' // c_null_char
 type(tm_struct) :: tm
 
-rc = strptime(trim(str)// c_null_char, trim(fmt)// c_null_char, tm)
-if(rc == 0) then
-  error stop "strptime failed"
-end if
+rc = strptime(trim(str)// c_null_char, fmt, tm)
+if(c_associated(rc, c_null_ptr)) error stop "strptime failed"
 
 print '(a3,1x,i4,1x,i2,1x,i2,1x,i2,1x,i2,1x,i2)', "OK:", &
   tm%tm_year+1900, tm%tm_mon+1, tm%tm_mday, tm%tm_hour, tm%tm_min, tm%tm_sec
