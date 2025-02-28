244fb21023a62f8819f545144451daa6b2a69bd0
scivision <scivision@users.noreply.github.com>
2025-02-27 21:34:00 -0500

    assume has f03 abstract

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -103,26 +103,6 @@ end program"
 f08contiguous
 )
 
-check_source_compiles(Fortran
-"program abst
-
-implicit none
-
-type, abstract :: L1
-integer, pointer :: bullseye(:,:)
-end type L1
-
-type, extends(L1) :: L2
-integer, pointer :: arrow(:)
-end type L2
-
-class(L2), allocatable :: obj
-
-end program
-"
-f03abstract
-)
-
 include(${CMAKE_CURRENT_LIST_DIR}/f08submod_bind.cmake)
 
 
