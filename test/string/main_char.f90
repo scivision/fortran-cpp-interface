824366dd698f1f1e7a63ab5bf0746bf9bf32aafb
scivision <scivision@users.noreply.github.com>
2026-04-29 20:27:02 -0400

    single_char: correct type

--- test/string/main_char.f90
+++ test/string/main_char.f90
@@ -6,17 +6,20 @@ use, intrinsic :: iso_fortran_env
 implicit none
 
 interface
-character(kind=c_char) function three() bind(C)
-import
+character(kind=C_CHAR, len=1) function three() bind(C)
+import C_CHAR
 end function
 end interface
 
-character :: fc
+character(kind=C_CHAR) :: fc
 
 fc = three()
 
 print '(i0,1x,a)', len_trim(fc), fc
 
-if (fc /= "3" .or. len_trim(fc) /= 1) error stop "ERROR: unexpected result from C function"
+if (fc /= C_CHAR_"3" .or. len_trim(fc) /= 1) then
+  write(error_unit, '(a,i0)') "ERROR: unexpected result from C function: " // fc // " length ", len_trim(fc)
+  error stop
+end if
 
 end program
