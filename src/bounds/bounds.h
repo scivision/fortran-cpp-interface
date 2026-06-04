0e477dc3c4d70faf08443d46e1c6248eed054a8c
scivision <scivision@users.noreply.github.com>
2026-06-04 01:23:55 -0400

    add lubounds test

---
+++ src/bounds/bounds.h
@@ -0,0 +1,6 @@
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ < 202311L
+#include <stdbool.h>
+#endif
+
+bool c_bounder(CFI_cdesc_t*, CFI_index_t, CFI_index_t);
+bool c_bounder_2d(CFI_cdesc_t*, CFI_index_t, CFI_index_t, CFI_index_t, CFI_index_t);
